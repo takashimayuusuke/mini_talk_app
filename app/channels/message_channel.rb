b805b5bb239f6239e482a49f1f1ff865720aaae8
takashimayuusuke <71985128+takashimayuusuke@users.noreply.github.com>
2021-02-08 18:33:09 +0900

    Revert "Action cableの実装"

--- app/channels/message_channel.rb
+++
@@ -1,9 +0,0 @@
-class MessageChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "message_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,5 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(text: params[:message][:text])
-    if @message.save
-      ActionCable.server.broadcast 'message_channel', content: @message
-    end
   end
 end
